31cf8111c616afaffad01c734c5edebd635f835d
Cimorexave <sadeq.s502@gmail.com>
2023-12-09 20:51:52 +0330

    basic types taught (armita was a baby)

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log('hello baby!');
+console.log('hello baby!')
+
+// let my_name = "sina";
+// const myAge = 25;
+
+// let myName: number
+let myBoobs: Array<string> = ["biba", "booba"]
+let myInfo: Object = {
+    'name': 'sina',
+    'age': 25,
+    'numberOfBoobs': 2,
+    'gender': 'male'
+}
+
+console.log(myInfo["name"]);
